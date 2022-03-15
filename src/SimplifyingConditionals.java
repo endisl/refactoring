e2d518fafe5fde51354ef5e92aac585a77d18440
endisl <endiluamba@gmail.com>
2022-03-14 21:31:18 -0300

    Refactoring

--- src/SimplifyingConditionals.java
+++ src/SimplifyingConditionals.java
@@ -1,6 +1,6 @@
 public class SimplifyingConditionals {
 
-    // 1. Consollidate Conditional Expression
+    // 1. Consolidate Conditional Expression
     double disabilityAmount() {
         if (isNotEligibleForDisability)
             return 0;
@@ -15,17 +15,57 @@ public class SimplifyingConditionals {
     send();
 
     // 3. Decompose Conditional
-
+    if (isSummer(date)) {
+        charge = summerCharge(quantity);
+    }
+    else {
+        charge = winterCharge(quantity);
+    }
 
     // 4. Remove Control Flag
     /* If a boolean variable acts as a control flag for multiple booelan expressions,
        use break, continue and return instead of the variable. */
 
     // 5. Replace Nested Conditional with Guard Clauses
-
+    public double getPayAmount() {
+        if (isDead) {
+            return deadAmount();
+        }
+        if (isSeparated) {
+            return separatedAmount();
+        }
+        if (isRetired) {
+            return retiredAmount();
+        }
+        return normalPayAmount();
+    }
 
     // 6. Replace Conditional with Polymorphism
+    abstract class Bird {
+        //...
+        abstract double getSpeed();
+    }
 
+    class European extends Bird {
+        double getSpeed() {
+            return getBaseSpeed();
+        }
+    }
+
+    class African extends Bird {
+        double getSpeed() {
+            return getBaseSpeed() - getLoadFactor * numberOfCoconuts;
+        }
+    }
+
+    class NorwegianBlue extends Bird {
+        double getSpeed() {
+            return (isNailed) ? 0 : getBaseSpeed(voltage);
+        }
+    }
+
+    //...client code
+    speed = bird.getSpeed();
 
     // 7. Introduce Null Object
 
